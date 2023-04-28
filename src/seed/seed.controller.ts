6814e5d83cd2f06a85eef7a0ffbf7fda6ff98f68
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2023-04-27 23:06:03 -0600

    Antes de insertar un lote

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
+
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+ 
 
   @Get()
-  executeSeed() {
+  async executeSeed() {
     return this.seedService.executeSeed();
   }
 
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+
+  private readonly axios: AxiosInstance = axios
   
-  executeSeed () {
-    return 'Seed excecuted';
+  async executeSeed () {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    data.results.forEach( ({ url,name }) => {
+
+      const segments = url.split('/');
+      const no: number = +segments[ segments.length - 2 ] 
+      console.log({ name, no })
+
+    })
+
+    return data.results;
   }
 
 }
